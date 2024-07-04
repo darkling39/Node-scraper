fe807971405f862c4e2fa5dcf3d7ce9b951977ac
Max Ivanov <axioma162@gmail.com>
2024-07-05 00:14:58 +0300

    Added Nodemon & Morgan

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express()
 
 const routes = require('./scraper')
 
 app.use('/products', routes)
+app.use(morgan('dev'))
+
 
 module.exports = app
