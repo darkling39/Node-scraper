6297f9c9c7cb987b13d0a54bcf53b7144d969eb5
Max Ivanov <axioma162@gmail.com>
2024-07-05 14:24:20 +0300

    Scraping and Error handling
    installed morgan & body parser
    
    app.js - added error handling
    srcaper.js -
    added scraping sunctionality
    now completed json object can be
    posted

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express()
+const bodyParser = require('body-parser');
 
 const routes = require('./scraper')
 
-app.use('/products', routes)
-app.use(morgan('dev'))
 
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.use('/products', routes)
+
+app.use((req,res,next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
--- scraper.js
+++ scraper.js
@@ -1,16 +1,57 @@
 const express = require('express');
+const cheerio = require('cheerio');
 
 const router = express.Router()
 
+
+async function getData() {
+    const url = 'https://rozetka.com.ua/ua/asus-90nr0jf7-m004n0/p418978512/'
+    const res = await fetch(url)
+
+    const html = await res.text()
+    const $ = cheerio.load(html)
+
+    const title = $('h1').text().toString().slice(0,256)
+    const subtitle = $('p.mt-4.ng-star-inserted').text().toString().slice(0,256)
+    const description = $('rz-text-content > div > div > div').text().toString().slice(0,2048)
+    const price = $('div.product-price__wrap.ng-star-inserted > p.product-price__big.product-price__big-color-red').text().toString()
+    let numPrice = Number(price.slice(0, price.length -1).replace(/\s/g, ''))
+    const specifications = {
+        garantee: $('rz-collapse-text > div > p').text()
+    }
+    const type = $('ul > li.breadcrumbs__item.breadcrumbs__item--last.ng-star-inserted > a > span').text()
+    const image = $('div > rz-gallery-main-content-image > img').toString()
+    let imgSrc = image.slice(image.lastIndexOf("src=\"") + 5, image.length).replace("\"", "")
+    const source = url.includes("rozetka") ? 'Rozetka' : 'Telemart'
+    const productsData = {
+        title: title,
+        subtitle: subtitle,
+        description: description,
+        price: numPrice,
+        specifications: specifications,
+        type: type,
+        image: imgSrc,
+        source: source
+    }
+    return productsData
+}
+
 router.get('/', (req, res, next) => {
-    res.status(200).json(
-        {message: 'get request to /products'}
-    )
+    res.status(200).json()
 })
 
-router.post('/', (req, res, next) => {
-    res.status(200).json(
-        {message: 'post request to /products'}
-    )
+router.post('/', async (req, res, next) => {
+    try{
+        const prod = await getData()
+
+        res.status(200).json(prod)
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "An error ocured",
+            error: error
+        })
+    }
 })
 module.exports = router
